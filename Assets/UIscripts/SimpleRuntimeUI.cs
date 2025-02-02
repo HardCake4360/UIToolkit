3c16f5b897760f55b64d420e736b1dce1e502516
HardCake4360 <chriskim4360@gmail.com>
2025-02-03 00:06:56 +0900

    2일차 커밋
    스크립트에서 몇개 해봄

--- Assets/UIscripts/SimpleRuntimeUI.cs
+++ Assets/UIscripts/SimpleRuntimeUI.cs
@@ -4,7 +4,10 @@ using UnityEngine.UIElements;
 public class SimpleRuntimeUI : MonoBehaviour
 {
     private Button _button;
+    private Button _button2;
     private Toggle _toggle;
+
+    private VisualElement _sprite;
 
     private int _clickCount;
 
@@ -15,9 +18,12 @@ public class SimpleRuntimeUI : MonoBehaviour
         var uiDocument = GetComponent<UIDocument>();
 
         _button = uiDocument.rootVisualElement.Q("button") as Button;
+        _button2 = uiDocument.rootVisualElement.Q("button2") as Button;
         _toggle = uiDocument.rootVisualElement.Q("toggle") as Toggle;
+        _sprite = uiDocument.rootVisualElement.Q("sprite") as VisualElement;
 
         _button.RegisterCallback<ClickEvent>(PrintClickMessage);
+        _button2.RegisterCallback<ClickEvent>(OnClick);
 
         var _inputFields = uiDocument.rootVisualElement.Q("input-message");
         _inputFields.RegisterCallback<ChangeEvent<string>>(InputMessage);
@@ -26,6 +32,7 @@ public class SimpleRuntimeUI : MonoBehaviour
     private void OnDisable()
     {
         _button.UnregisterCallback<ClickEvent>(PrintClickMessage);
+        _button2.UnregisterCallback<ClickEvent>(OnClick);
     }
 
     private void PrintClickMessage(ClickEvent evt)
@@ -34,6 +41,11 @@ public class SimpleRuntimeUI : MonoBehaviour
 
         Debug.Log($"{"button"} was clicked!" +
                 (_toggle.value ? " Count:" + _clickCount : ""));
+    }
+    private void OnClick(ClickEvent evt)
+    {
+        Debug.Log("sdjf;alkjdas;lfkjsa;dlfj");
+        _sprite.SetEnabled(false);
     }
 
     public static void InputMessage(ChangeEvent<string> evt)
